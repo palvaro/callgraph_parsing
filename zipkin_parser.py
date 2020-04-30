f215aa23f8840df1a70f4ea36611087acf0f0a46
Peter Alvaro <palvaro@gmail.com>
2020-04-30 14:22:55 -0700

    zipkin

--- zipkin_parser.py
+++ zipkin_parser.py
@@ -1,5 +1,7 @@
 from canonical_tree import CallGraph
+from abstract_graphs import DAG
 from graphviz import Digraph
+from frozendict import frozendict
 import json
 
 def draw_graph(tree, name, rule):
@@ -7,61 +9,49 @@ def draw_graph(tree, name, rule):
     dotfile = tree.todot(dot, rule)
     dot.render(name, view=True)
 
-class JaegerParser():
+class ZipkinParser():
     def __init__(self, file):
         fp = open(file, "r")
         self.jsn = json.loads(fp.read())
         fp.close()
     
         self.map = {}
+        self.edges = set()
        
     def process(self):
-        #data = self.jsn['data']
-
-        #processes = {}
-        #for process in data[0]['processes']:
-        #    processes[process] = data[0]['processes'][process]
-
-        #for span in data[0]['spans']:
         for span in self.jsn:
             print("SPAN %s" % span)            
             labels = {}
             for key in span:
                 t = type(span[key])
                 if t in (str, int, float):
-                    #print("yay %s" % key)
                     labels[key] = span[key]
-                #else:
-                #    print("nay %s - %s" % (key, t))
-            #for tag in span['tags']:
-            #    labels[tag['key']] = tag['value']
 
-            #if 'references' in span:
-            #    for item in span['references']:
-            #        if item['refType'] == "CHILD_OF":
-            #            labels["parent_span"] = item['spanID']
-
-            #labels['serviceName'] = processes[span['processID']]['serviceName']
-            #for tag in processes[span['processID']]['tags']:
-            #    labels['process-' + tag['key']] = tag['value']
-
-            node = CallGraph(labels)
-            self.map[span['id']] = node
+            
+            self.map[span['id']] = labels
 
         for key in self.map:
-            if 'parentId' in self.map[key].labels:
-                parent_key = self.map[key].labels['parentId']#.encode('ascii', 'ignore')
+            if 'parentId' in self.map[key]:
+                parent_key = self.map[key]['parentId']#.encode('ascii', 'ignore')
                 parent = self.map[parent_key]
-                parent.add_child(self.map[key])
-            else:
-                self.root = self.map[key]
+                #parent.add_child(self.map[key])
+                self.edges.add((frozendict(self.map[key]),  frozendict(labels)))
 
 
-        return self.root
 
-p = JaegerParser("catrace0.json")
+    def to_abstract(self):
+        dag = DAG()
+        for l, r in self.edges:
+            dag.add_edge(l, r)
+        return dag
+            
+
+p = ZipkinParser("catrace0.json")
 p.process()
+dag = p.to_abstract()
 
-draw_graph(p.root, "foo", [])
+
+dag.todot("abs")
+
 
 
