53876434677d295404219573720e3f20b7ccf0e7
Peter Alvaro <palvaro@gmail.com>
2020-05-01 16:49:28 -0700

    flush

--- zipkin_parser.py
+++ zipkin_parser.py
@@ -20,37 +20,24 @@ class ZipkinParser():
        
     def process(self):
         for span in self.jsn:
-            print("SPAN %s" % span)            
             labels = {}
             for key in span:
                 t = type(span[key])
                 if t in (str, int, float):
                     labels[key] = span[key]
-
-            
             self.map[span['id']] = labels
     
-        print("I have %d spans" % len(self.map.keys()))
         for key in self.map:
             if 'parentId' in self.map[key]:
-                print("YES %s" % self.map[key]["id"])
                 parent_key = self.map[key]['parentId']
-                print("parent key is %s" % parent_key)
                 parent = self.map[parent_key]
-                #parent.add_child(self.map[key])
-                #self.edges.add((frozendict(self.map[key]),  frozendict(labels)))
                 pair = (frozendict(parent),  frozendict(self.map[key]))
-                print("PAIR IS %s %s" % pair)
                 self.edges.add(pair)
-            else:
-                print("NO parent for %s" % self.map[key])
-
 
 
     def to_abstract(self):
         dag = DAG()
         for l, r in self.edges:
-            print("EDg")
             dag.add_edge(l, r)
         return dag
             
